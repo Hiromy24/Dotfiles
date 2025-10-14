3fa55b4a1b19feed8e88b5665d528886b00d2840
Hiromy24 <henryillescas24@gmail.com>
2025-10-14 17:16:46 +0200

    added packages list

---
+++ hiromy/install_pkg.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+pacman_packages=(
+	hyprland hyprshade swww hyprlock grim slurp waybar rofi rofi-emoji hyprshot xdg-desktop-portal-hyprland xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk
+	
+	brightnessctl network-manager-applet bluez bluez-utils blueman pipewire wireplumber pavucontrol
+
+	ghostty cava 
+
+	sddm qt5ct qt6ct qt5-wayland qt6-wayland
+
+	discord firefox
+
+	ttf-jetbrains-mono-nerd noto-fonts nwg-look adw-gtk-theme kvantum-qt5 libvips cliphist gnome-characters keepass
+)
+aur_packages=(
+	wlogout
+
+	spotify
+	
+	visual-studio-code-bin
+
+	ttf-segoe-ui-variable tint
+)
+
+sudo pacman -S --noconfirm "${pacman_packages[@]}"
+yay -S --noconfirm "${aur_packages[@]}"
+
